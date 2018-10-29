145d90fb51f3af8faf679cece5bac22ad51e767a
Benjadata96 <44429700+Benjadata96@users.noreply.github.com>
2018-10-29 11:11:42 +0100

    Update Model_Training_20News.py

--- Model_Training_20News.py
+++ Model_Training_20News.py
@@ -1,10 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Oct 29 10:44:40 2018
-
-@author: BenjaminSalem
-"""
 import numpy
 import torch
 from torch.autograd import Variable
